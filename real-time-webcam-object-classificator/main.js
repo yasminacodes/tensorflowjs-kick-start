b6bf077299937e0bfa4bb35a95ef2918205bc51a
Yasmina <yasminacodes@hotmail.com>
2023-07-06 09:13:05 +0200

    new project added

---
+++ real-time-webcam-object-classificator/main.js
@@ -0,0 +1,64 @@
+const video = document.getElementById('webcam');
+const liveView = document.getElementById('liveView');
+
+function getUserMediaSupported() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+if (!getUserMediaSupported()) {
+  console.warn('getUserMedia() is not supported by your browser')
+  alert("This program is not supported by your browser")
+}
+
+const constraints = {
+  video: true
+};
+
+navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+  video.srcObject = stream;
+  video.addEventListener('loadeddata', predictWebcam)
+})
+
+
+var model = undefined
+
+cocoSsd.load().then(function (loadedModel) {
+  model = loadedModel
+})
+
+var children = []
+
+function predictWebcam() {
+  let timerInterval = setInterval(() => {
+    model.detect(video).then(function (predictions) {
+      for (let i = 0; i < children.length; i++) {
+        liveView.removeChild(children[i])
+      }
+      children.splice(0)
+
+      for (let n = 0; n < predictions.length; n++) {
+        if (predictions[n].score > 0.06) {
+          const p = document.createElement('p')
+          p.innerText = predictions[n].class + ' - with ' + Math.round(parseFloat(predictions[n].score) * 100) + '% confidence.';
+          p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: ' + (predictions[n].bbox[1] - 10) + 'px; width: ' + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;'
+
+          const highlighter = document.createElement("div")
+          highlighter.setAttribute("class", "highlighter")
+          highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: ' + predictions[n].bbox[1] + 'px; width: ' +
+            predictions[n].bbox[2] + 'px; height: ' +
+            predictions[n].bbox[3] + 'px;';
+          liveView.appendChild(highlighter);
+          liveView.appendChild(p)
+          children.push(highlighter)
+          children.push(p)
+        }
+      }
+
+    })
+  }, 250)
+}
+
+
+function setup() {
+  canvas(window.innerWidth, window.innerHeight);
+}
